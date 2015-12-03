f6ebdd5d5977578158ff64f7dd25201fa5856612
Steffen Hirschmann <Steffen.Hirschmann@ipvs.uni-stuttgart.de>
2015-12-03 13:29:36 +0100

    Fix copy paste error.

--- mit.py
+++ mit.py
@@ -8,7 +8,7 @@ def mortonIter2D(end, start=0):
         yield morton.d2xy(i)
 
 def mortonIter3D(end, start=0):
-    """Simple generator for two-dimensional indices along the Z curve from
+    """Simple generator for three-dimensional indices along the Z curve from
     morton indices [start, end)---start included, end excluded."""
     for i in xrange(start, end):
         yield morton.d2xyz(i)
